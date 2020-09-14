29dd707e5a9b849b27d316047ccde9ea24fc5f40
EBenezer Gakuo <gakuoebenezer@gmail.com>
2020-09-14 10:50:12 +0300

    Articles base url added

--- app/requests.py
+++ app/requests.py
@@ -10,7 +10,7 @@ def configure_request(app):
   global api_key,base_url
   api_key =app.config['NEWS_API_KEY']
   base_url =app.config['NEWS_API_BASE_URL']
-  
+  # article_url =app.config[]
   
   
 def get_sources(category):
