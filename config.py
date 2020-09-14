385ec5a865ac2b24c9b7760dc4c3a1a7d7c7c958
EBenezer Gakuo <gakuoebenezer@gmail.com>
2020-09-14 10:03:16 +0300

    for loop removed

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-  NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?country=us&category={}&apiKey={}'
+  NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?&category={}&apiKey={}'
   
   NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
   
