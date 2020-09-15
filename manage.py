9ce48ce5f3b9bc6408d5e3541ee54c318d34f9d2
EBenezer Gakuo <gakuoebenezer@gmail.com>
2020-09-15 14:03:42 +0300

    tests running

--- manage.py
+++ manage.py
@@ -7,7 +7,13 @@ app = create_app('development')
 
 manager = Manager(app)
 manager.add_command('server',Server)
+@manager.command
+def tests():
+    """Run unittests"""
+    import unittest
+    tests =unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
 
-manager.add_command('server',Server)
+
 if __name__ == '__main__':
     manager.run()
--- tests/tests_article.py
+++ tests/tests_article.py
@@ -8,3 +8,7 @@ class ArticlesTest(unittest.TestCase):
     
   def test_instance(self):
     self.assertTrue(isinstance(self.new_article,Articles))  
+    
+    
+if __name__ == "__main__":
+      unittest.main()   
